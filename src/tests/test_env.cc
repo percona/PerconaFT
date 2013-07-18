043e497ba6704a63ade4074e148f4ff5d3ae5923
Rich Prohaska <prohaska@tokutek.com>
2013-07-18 10:52:03 -0400

    #7 test to dump the test programming env

--- src/tests/test_env.cc
+++ src/tests/test_env.cc
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "toku_path.h"
 
+// this test dumps out the programming environment.
+
 int main(int argc, char *const argv[], char *const env[]) {
     for (int i = 0; i < argc; i++)
         printf("argv[%d]: %s\n", i, argv[i]);
