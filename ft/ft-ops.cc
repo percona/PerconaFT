bfb45453f5c7e5b3740a24e4bcab3339fce44ac4
Zardosht Kasheff <zardosht@gmail.com>
2013-09-11 15:30:51 -0400

    fix bug

--- ft/ft-ops.cc
+++ ft/ft-ops.cc
@@ -1658,7 +1658,7 @@ toku_ft_bn_apply_cmd_once (
         &numbytes_delta
         );
 
-    newsize = leafentry_disksize(new_le);
+    newsize = new_le ? leafentry_disksize(new_le) : 0;
     if (le && new_le) {
         workdone_this_le = (oldsize > newsize ? oldsize : newsize);  // work done is max of le size before and after message application
 
