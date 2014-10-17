646ebfaf3eb6dfa3451e9b0ecff48a5ddcbaac06
Leif Walsh <leif.walsh@gmail.com>
2014-10-17 16:32:53 -0400

    use read only txn when creating an autotxn for a cursor

--- ftcxx/cursor.cpp
+++ ftcxx/cursor.cpp
@@ -17,7 +17,7 @@ namespace ftcxx {
         if (db.db() != nullptr) {
             DB_TXN *txnp = txn.txn();
             if (txnp == nullptr) {
-                _txn = DBTxn(DBEnv(db.db()->dbenv));
+                _txn = DBTxn(DBEnv(db.db()->dbenv), DB_TXN_READ_ONLY | DB_READ_COMMITTED);
                 txnp = _txn.txn();
             }
 
