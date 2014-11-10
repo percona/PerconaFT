37f7c8708a0b993d7348afd6d68976fa06698445
John Esmet <john.esmet@gmail.com>
2014-11-10 13:11:11 -0500

    Move code out of toku_crash.h

--- portability/toku_crash.cc
+++ portability/toku_crash.cc
@@ -88,7 +88,9 @@ PATENT RIGHTS GRANT:
 
 #ident "Copyright (c) 2007-2013 Tokutek Inc.  All rights reserved."
 
+#include "portability/toku_compiler.h"
 #include "portability/toku_crash.h"
+#include "portability/toku_stdlib.h"
 
 #if TOKU_WINDOWS
 
@@ -106,8 +108,102 @@ PATENT RIGHTS GRANT:
 
 #include "portability/toku_atomic.h"
 #include "portability/toku_os.h"
-#include "portability/toku_pthread.h"
 #include "portability/toku_race_tools.h"
+
+//Simulate as hard a crash as possible.
+//Choices:
+//  raise(SIGABRT)
+//  kill -SIGKILL $pid
+//  divide by 0
+//  null dereference
+//  abort()
+//  assert(false) (from <assert.h>)
+//  assert(false) (from <toku_assert.h>)
+//
+//Linux:
+//  abort() and both assert(false) cause FILE buffers to be flushed and written to disk: Unacceptable
+//
+//kill -SIGKILL $pid is annoying (and so far untested)
+//
+//raise(SIGABRT) has the downside that perhaps it could be caught?
+//I'm choosing raise(SIGABRT), followed by divide by 0, followed by null dereference, followed by all the others just in case one gets caught.
+NORETURN
+void toku_hard_crash_on_purpose(void) {
+    raise(SIGKILL); //Does not flush buffers on linux; cannot be caught.
+    {
+        int zero = 0;
+        int infinity = 1/zero;
+        fprintf(stderr, "Force use of %d\n", infinity);
+        fflush(stderr); //Make certain the string is calculated.
+    }
+    {
+        void * intothevoid = NULL;
+        (*(int*)intothevoid)++;
+        fprintf(stderr, "Force use of *(%p) = %d\n", intothevoid, *(int*)intothevoid);
+        fflush(stderr);
+    }
+    abort();
+    fprintf(stderr, "This line should never be printed\n");
+    fflush(stderr);
+}
+
+// Similar to toku_hard_crash_on_purpose, but the goal isn't to crash hard, the primary goal is to get a corefile, the secondary goal is to terminate in any way possible.
+// We don't really care if buffers get flushed etc, in fact they may as well flush since there may be useful output in stdout or stderr.
+//
+// By default, the following signals generate cores:
+//  Linux, from signal(7):
+//     SIGQUIT       3       Core
+//     SIGILL        4       Core
+//     SIGABRT       6       Core
+//     SIGFPE        8       Core
+//     SIGSEGV      11       Core
+//
+//  Darwin and FreeBSD, from signal(3):
+//     3     SIGQUIT      create core image
+//     4     SIGILL       create core image
+//     5     SIGTRAP      create core image
+//     6     SIGABRT      create core image
+//     7     SIGEMT       create core image
+//     8     SIGFPE       create core image
+//     10    SIGBUS       create core image
+//     11    SIGSEGV      create core image
+//     12    SIGSYS       create core image
+//
+// We'll raise these in some sequence (common ones first), then try emulating the things that would cause these signals to be raised, then eventually just try to die normally and then loop like abort does.
+// Start with a toku assert because that hopefully prints a stacktrace.
+NORETURN
+void toku_crash_and_dump_core_on_purpose(void) {
+    assert(false);
+    invariant(0);
+    raise(SIGQUIT);
+    raise(SIGILL);
+    raise(SIGABRT);
+    raise(SIGFPE);
+    raise(SIGSEGV);
+#if defined(__FreeBSD__) || defined(__APPLE__)
+    raise(SIGTRAP);
+    raise(SIGEMT);
+    raise(SIGBUS);
+    raise(SIGSYS);
+#endif
+    abort();
+    {
+        int zero = 0;
+        int infinity = 1/zero;
+        fprintf(stderr, "Force use of %d\n", infinity);
+        fflush(stderr); //Make certain the string is calculated.
+    }
+    {
+        void * intothevoid = NULL;
+        (*(int*)intothevoid)++;
+        fprintf(stderr, "Force use of *(%p) = %d\n", intothevoid, *(int*)intothevoid);
+        fflush(stderr);
+    }
+    raise(SIGKILL);
+    while (true) {
+        // don't return
+    }
+}
 
 enum { MAX_GDB_ARGS = 128 };
 
@@ -221,5 +317,4 @@ toku_try_gdb_stack_trace(const char *gdb_path) {
     }
 }
 
-#endif
-
+#endif // !TOKU_WINDOWS
