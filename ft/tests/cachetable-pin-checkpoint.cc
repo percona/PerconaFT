18649a32a21b934f302ee3633fef53dd1eee86c3
Jun <junyuan@cs.stonybrook.edu>
2017-09-19 15:16:41 -0400

    [TDB-20] With checkpoint disabled, closing FT file still rotates header.
                    This is an attempt based on @BohuTang's patch
                    https://github.com/percona/PerconaFT/pull/331
    
                    in particular, the ctest is all his credits.
    
                    Summary:
                    1) The issue is real.
                    2) @Bohu's patch works for mediating the issue but somewhat introduces
                    a ft leak that the ft with ref=0 may be left until next open/close or
                    checkpoint.
                    3) If ft_close should be held off upon a backup, then it simply means
                    a backup should hold a ref to the ft and responsible for closing ft up
                    if it is the last one to hold it, like any other possible referencer.
                    including the checkpoint, the txn and ft handlers.
                    4) mtr test contributed by @BohuTang still passes

--- ft/tests/cachetable-pin-checkpoint.cc
+++ ft/tests/cachetable-pin-checkpoint.cc
@@ -364,21 +364,15 @@ cachetable_test (void) {
     toku_cachetable_create(&ct, test_limit, ZERO_LSN, nullptr);
     const char *fname1 = TOKU_TEST_FILENAME;
     unlink(fname1);
-    r = toku_cachetable_openf(&f1, ct, fname1, O_RDWR|O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO); assert(r == 0);
+    r = toku_cachetable_openf(&f1, ct, fname1, O_RDWR | O_CREAT,
+                              S_IRWXU | S_IRWXG | S_IRWXO);
+    assert(r == 0);
 
     toku_cachefile_set_userdata(
-        f1,
-        NULL,
-        &dummy_log_fassociate,
-        &dummy_close_usr,
-        &dummy_free_usr,
-        &dummy_chckpnt_usr,
-        &test_begin_checkpoint,
-        &dummy_end,
-        &dummy_note_pin,
-        &dummy_note_unpin
-        );
-    
+        f1, NULL, &dummy_log_fassociate, &dummy_close_usr, &dummy_free_usr,
+        &dummy_chckpnt_usr, &test_begin_checkpoint, &dummy_end, &dummy_note_pin,
+        &dummy_note_unpin, &dummy_note_pin, &dummy_note_unpin);
+
     toku_pthread_t time_tid;
     toku_pthread_t checkpoint_tid;
     toku_pthread_t move_tid[NUM_MOVER_THREADS];
